e52f872936e7c944a17939917595253ae1b5a4e9
chuan_wuhao <443547225@qq.com>
2022-08-29 11:51:51 +0800

    Fix the problem of hot update error report caused by transition components

--- src/components/RayTransitionComponent/index.vue
+++ src/components/RayTransitionComponent/index.vue
@@ -8,8 +8,8 @@
   </router-view>
 </template>
 <script lang="ts" setup>
-// 带过渡动画 RouterView 组件
+// 带过渡动画 `RouterView` 组件
 // 如果子路由需要做动画切换，则需要此组件
-// 为什么必须为 vue 文件，因为 tsx 文件在解析的时候会抛出警告不好看
-// 只需要像使用 RouterView 组件时一样使用即可, 但是不能对于子路由生效, 所以需要在子路由显示的地方替换 RouterView 组件
+// 为什么必须为 `vue` 文件，因为 `tsx` 文件在解析的时候会抛出警告不好看
+// 只需要像使用 `RouterView` 组件时一样使用即可, 但是不能对于子路由生效, 所以需要在子路由显示的地方替换 `RouterView` 组件
 </script>
