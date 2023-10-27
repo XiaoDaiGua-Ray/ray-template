6295e38ca5b27fe2b8917c0903dced5b13ed7bc3
XiaoDaiGua-Ray <443547225@qq.com>
2023-10-27 14:56:24 +0800

    v4.2.8

--- mockServer/mock-data.js
+++ mockServer/mock-data.js
@@ -4,7 +4,7 @@ var __export = (target, all) => {
     __defProp(target, name, { get: all[name], enumerable: true });
 };
 
-// mock-data-1698226243058.js
+// mock-data-1698389655095.js
 import { transformMockData } from "vite-plugin-mock-dev-server";
 
 // mock/demo/person.mock.ts
@@ -85,7 +85,7 @@ var getPersonList = defineMock({
   }
 });
 
-// mock-data-1698226243058.js
+// mock-data-1698389655095.js
 var exporters = [person_mock_exports];
 var mockList = exporters.map((raw) => {
   let mockConfig;
@@ -99,7 +99,7 @@ var mockList = exporters.map((raw) => {
   }
   return mockConfig;
 });
-var mock_data_1698226243058_default = transformMockData(mockList);
+var mock_data_1698389655095_default = transformMockData(mockList);
 export {
-  mock_data_1698226243058_default as default
+  mock_data_1698389655095_default as default
 };
