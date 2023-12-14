66cf689d708d9b8060bd452ae9d39f7b2746329b
XiaoDaiGua-Ray <443547225@qq.com>
2023-12-14 09:47:13 +0800

    chore: 更新commitlint配置，添加husky配置，实现commitlint检查提交信息

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,20 +1,38 @@
+// update: 更新代码 | Update code
+// fix: 修复 bug | Fix bug
+// feat: 新功能 | New feature
+// chore: 构建过程或辅助工具的变动 | Build process or auxiliary tool changes
+// docs: 文档 | Documentation
+// refactor: 重构（即不是新增功能，也不是修改 bug 的代码变动） | Refactor (i.e. code changes that are neither new features nor bug fixes)
+// test: 增加测试 | Add test
+// style: 代码格式（不影响功能，例如空格、分号等格式修正） | Code format (no functional impact, such as space, semicolon, etc.)
+// version: 更新迭代 package.json 版本号 | Update the package.json version number
+// build: 构建 | Build
+
 module.exports = {
+  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
     'type-enum': [
       2,
       'always',
       [
-        'bug',
-        'feat',
+        'update',
         'fix',
+        'feat',
+        'chore',
         'docs',
-        'style',
         'refactor',
         'test',
-        'chore',
-        'revert',
-        'merge',
+        'style',
+        'version',
+        'build',
       ],
     ],
   },
