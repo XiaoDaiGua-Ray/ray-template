f9481fe2452aaef2a495a805d50033bba3e18860
XiaoDaiGua-Ray <51957438+XiaoDaiGua-Ray@users.noreply.github.com>
2024-01-12 13:09:21 +0000

    Deploying to dist from @ XiaoDaiGua-Ray/ray-template@57ed469543e75075af3cfaf078ed2ca49e177683 🚀

--- mockServer/mock-data.js
+++ mockServer/mock-data.js
@@ -4,7 +4,7 @@ var __export = (target, all) => {
     __defProp(target, name, { get: all[name], enumerable: true });
 };
 
-// mock-data-1704938572042.js
+// mock-data-1705064959334.js
 import { transformMockData } from "vite-plugin-mock-dev-server";
 
 // mock/demo/person.mock.ts
@@ -85,7 +85,7 @@ var getPersonList = defineMock({
   }
 });
 
-// mock-data-1704938572042.js
+// mock-data-1705064959334.js
 var exporters = [person_mock_exports];
 var mockList = exporters.map((raw) => {
   let mockConfig;
@@ -99,7 +99,7 @@ var mockList = exporters.map((raw) => {
   }
   return mockConfig;
 });
-var mock_data_1704938572042_default = transformMockData(mockList);
+var mock_data_1705064959334_default = transformMockData(mockList);
 export {
-  mock_data_1704938572042_default as default
+  mock_data_1705064959334_default as default
 };
