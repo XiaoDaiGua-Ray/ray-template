adf19a95339b2050474cb41cca9862f17f0659a3
XiaoDaiGua-Ray <51957438+XiaoDaiGua-Ray@users.noreply.github.com>
2025-08-09 11:07:20 +0000

    Deploying to dist from @ XiaoDaiGua-Ray/ray-template@49af61a339223ec6344045eb140b418a286acc64 🚀

---
+++ js/@css-render_vue3-ssr@0.15.14_vue@3.5.17_typescript@5.8.3_-OHQSPFRS.js
@@ -0,0 +1,3 @@
+import{i as u}from"./@vue_runtime-core@3.5.17-YYjRSOhw.js";const i="@css-render/vue3-ssr";function o(e,n){return`<style cssr-id="${e}">
+${n}
+</style>`}function c(e,n,t){const{styles:r,ids:s}=t;s.has(e)||r!==null&&(s.add(e),r.push(o(e,n)))}const d=typeof document<"u";function l(){if(d)return;const e=u(i,null);if(e!==null)return{adapter:(n,t)=>c(n,t,e),context:e}}export{l as u};
